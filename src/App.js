6dcef1641d74ee0284c25c4af66279761d62a84e
milicav24 <36275087+milicav24@users.noreply.github.com>
2018-03-03 09:41:06 +0100

    Hangman

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MyCustomComponent from './MyCustomComponent';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="frame">
+      <p className="text">Hang Man</p>
+        <MyCustomComponent />
       </div>
     );
   }
 }
-
 export default App;
